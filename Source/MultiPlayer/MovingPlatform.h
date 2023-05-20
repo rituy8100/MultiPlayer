db726c7ad7103bbf347132da20b83338d3c4b335
rituy8100 <69376319+rituy8100@users.noreply.github.com>
2023-05-21 00:53:00 +0900

    Authority Replicate

---
+++ Source/MultiPlayer/MovingPlatform.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "MovingPlatform.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYER_API AMovingPlatform : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+public:
+	AMovingPlatform();
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void BeginPlay() override;
+private:
+	UPROPERTY(EditAnywhere)
+		float Speed = 10;
+};
