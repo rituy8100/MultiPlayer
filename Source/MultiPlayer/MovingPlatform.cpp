1076f305957ae51982acea074aaf411c1561a8ad
rituy8100 <69376319+rituy8100@users.noreply.github.com>
2023-05-23 15:50:40 +0900

    PlaformTrigger

--- Source/MultiPlayer/MovingPlatform.cpp
+++ Source/MultiPlayer/MovingPlatform.cpp
@@ -17,16 +17,49 @@ void AMovingPlatform::BeginPlay()
 		SetReplicates(true);
 		SetReplicateMovement(true);
 	}
+
+	GlobalStartLocation = GetActorLocation();
+	//local vector을 global vector로 변환
+	GlobalTargetLocation= GetTransform().TransformPosition(TargetLocation);
 }
 
 void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	if (HasAuthority()) {
-		FVector Location = GetActorLocation();
-		Location += FVector(Speed * DeltaTime, 0, 0);
+	if (ActiveTriggers > 0) {
+		if (HasAuthority()) {
+			FVector Location = GetActorLocation();
+			//TargetLocation까지의 거리
+			float JournyLength = (GlobalTargetLocation - GlobalStartLocation).Size();
+			//현재 이동한 거리
+			float JourneyTraveled = (Location - GlobalStartLocation).Size();
 
-		SetActorLocation(Location);
+			//TargetLocation까지 도착했을경우 swap
+			if (JournyLength <= JourneyTraveled) {
+				FVector Swap = GlobalStartLocation;
+				GlobalStartLocation = GlobalTargetLocation;
+				GlobalTargetLocation = Swap;
+			}
+			//방향vector 정규화
+			FVector Direction = (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal();
+			//targetLocation으로 이동
+			Location += Speed * DeltaTime * Direction;
+
+			SetActorLocation(Location);
+		}
+	}
+}
+
+void AMovingPlatform::AddActiveTrigger()
+{
+	ActiveTriggers++;
+}
+
+void AMovingPlatform::RemoveActiveTrigger()
+{
+	if (ActiveTriggers > 0)
+	{
+		ActiveTriggers--;
 	}
 }
--- Source/MultiPlayer/MovingPlatform.h
+++ Source/MultiPlayer/MovingPlatform.h
@@ -20,7 +20,19 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Meta = (MakeEditWidget = true))
+		FVector TargetLocation;
+
+	void AddActiveTrigger();
+	void RemoveActiveTrigger();
 private:
 	UPROPERTY(EditAnywhere)
-		float Speed = 10;
+		float Speed = 50;
+
+	FVector GlobalStartLocation;
+	FVector GlobalTargetLocation;
+
+	UPROPERTY(EditAnywhere)
+		int ActiveTriggers = 1;
 };
