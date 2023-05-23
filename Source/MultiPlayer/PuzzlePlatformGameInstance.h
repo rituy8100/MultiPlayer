d752a38a01082b21d1f5ff276e4d85f7e06ea33e
rituy8100 <69376319+rituy8100@users.noreply.github.com>
2023-05-23 21:39:17 +0900

    Hosting Joining

---
+++ Source/MultiPlayer/PuzzlePlatformGameInstance.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "PuzzlePlatformGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYER_API UPuzzlePlatformGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+public:
+	UPuzzlePlatformGameInstance(const FObjectInitializer& ObjectInitializer);
+
+	virtual void Init();
+
+	UFUNCTION(Exec)
+		void Host();
+
+	UFUNCTION(Exec)
+		void Join(const FString& Address);
+};
